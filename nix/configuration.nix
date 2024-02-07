d3281694044ed9bda936923030f70bd8722890a6
Dylan Josef Sumser <dylansumser@googlemail.com>
2024-02-07 20:28:40 -0300

    add docker support

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -72,11 +72,13 @@ in {
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  ## Enable Docker
+  virtualisation.docker.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.djs = {
     isNormalUser = true;
     description = "Dylan Josef Sumser";
-    extraGroups = ["networkmanager" "wheel"];
+    extraGroups = ["networkmanager" "wheel" "docker"];
   };
 
   home-manager.useGlobalPkgs = true;
